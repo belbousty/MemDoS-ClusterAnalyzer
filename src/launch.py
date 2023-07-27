084947d8d073a4a3ecb8cb2c843bce26927233bb
belbousty <belbousty02@gmail.com>
2023-07-27 10:49:10 +0200

    Correcting command launching in pods

--- src/launch.py
+++ src/launch.py
@@ -1,17 +1,22 @@
 import argparse
 import utils
 import threading
-
+import shlex
 
 def launch_victims_apps(pod_name):
     app = utils.get_experiment_info('app')
     benchmark = app[pod_name]['benchmark']
     workload = app[pod_name]['workload']
 
+    print(f"    [+] Starting preparation for {workload} {benchmark} for {pod_name}")
     utils.exec_command_in_pod(pod_name,
-                f"/Hibench/bin/workloads/{workload}/{benchmark}/prepare/prepare.sh")
+                f"/HiBench/bin/workloads/{workload}/{benchmark}/prepare/prepare.sh")
+    print(f"    [+] Preparation Ended for {workload} {benchmark}")
+    
+    print(f"    [+] Running {workload} {benchmark} for {pod_name}")
     utils.exec_command_in_pod(pod_name,
-                f"/Hibench/bin/workloads/{workload}/{benchmark}/hadoop/run.sh")
+                f"/HiBench/bin/workloads/{workload}/{benchmark}/hadoop/run.sh")
+    print(f"    [+] Application ended for {pod_name}")
     pass
 
 def launch_attacks(pod_name, attack):
@@ -26,9 +31,10 @@ def launch_attacks(pod_name, attack):
 
     duration = utils.get_experiment_info('duration')
     pod_duration = int(duration[pod_name])*60
-    print(f" sleep {pod_start} && timeout {pod_duration} ./root/{binary}")
+    print(f"[+] Starting {pod_name} attack")
     utils.exec_command_in_pod(pod_name,
-                f" sleep {pod_start} && timeout {pod_duration} ./root/{binary}")
+                f'/bin/sh -c "sleep {pod_start} && timeout {pod_duration} ./root/{binary}"')
+    print(f"[+] {pod_name} attack ended")
 
 ##
 # duration sould be at least bigger than the biggest attack duration
@@ -38,6 +44,7 @@ def launch_experiment(duration, namespace='default'):
     for pod_name in pods:
         # Launch apps first
         if (utils.check_pod_name(pod_name, 'victim')):
+            print(f'[+] Starting app for {pod_name}')
             launch_victims_apps(pod_name)
     attack_threads = []
     for pod_name in pods:
@@ -60,5 +67,5 @@ if __name__ == '__main__':
     parser.add_argument("--duration", type=int, help="Expirement duration in minutes", default= 10, required=True)
 
     args = parser.parse_args()
-    #launch_experiment(args.duration)
+    launch_experiment(args.duration)
     pass
